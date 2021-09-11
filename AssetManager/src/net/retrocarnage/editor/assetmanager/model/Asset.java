8017526085057d01224cdfd71f0464e321a79266
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2021-09-11 09:03:26 +0200

    Refactored AssetManager model

---
+++ AssetManager/src/net/retrocarnage/editor/assetmanager/model/Asset.java
@@ -0,0 +1,106 @@
+package net.retrocarnage.editor.assetmanager.model;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.retrocarnage.editor.core.ApplicationFolderService;
+import org.apache.commons.io.IOUtils;
+
+/**
+ * An asset that can be managed by the AssetManager.
+ *
+ * @author Thomas Werner
+ */
+public abstract class Asset<T> {
+
+    private static final Logger logger = Logger.getLogger(Music.class.getName());
+
+    private String id;
+    private AttributionData attributionData;
+    private String name;
+    private String relativePath;
+    private List<String> tags;
+
+    protected Asset() {
+        this.tags = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public AttributionData getAttributionData() {
+        return attributionData;
+    }
+
+    public void setAttributionData(AttributionData attributionData) {
+        this.attributionData = attributionData;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRelativePath() {
+        return relativePath;
+    }
+
+    public void setRelativePath(String relativePath) {
+        this.relativePath = relativePath;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(final List<String> tags) {
+        this.tags = tags;
+    }
+
+    /**
+     * Reads the file associated with this asset.
+     *
+     * @param out destination
+     * @throws IOException
+     */
+    public void getData(final OutputStream out) throws IOException {
+        final ApplicationFolderService appFolderService = ApplicationFolderService.getDefault();
+        final Path appFolderPath = appFolderService.getApplicationFolder();
+        final Path filePath = Paths.get(appFolderPath.toString(), getRelativePath());
+        if (filePath.toFile().exists()) {
+            try (final InputStream in = new BufferedInputStream(Files.newInputStream(filePath))) {
+                IOUtils.copy(in, out);
+            } catch (IOException ex) {
+                logger.log(Level.WARNING, "Failed to read asset: {0}", filePath.toString());
+                throw new IOException("Failed to read asset " + filePath.toString(), ex);
+            }
+        } else {
+            logger.log(Level.WARNING, "Missing asset: {0}", filePath.toString());
+            throw new IOException("File not found: " + filePath.toString());
+        }
+    }
+
+    /**
+     * Creates a deep copy of this object.
+     *
+     * @return the copy
+     */
+    public abstract T deepCopy();
+
+}
--- AssetManager/src/net/retrocarnage/editor/assetmanager/model/Sprite.java
+++ AssetManager/src/net/retrocarnage/editor/assetmanager/model/Sprite.java
@@ -10,8 +10,6 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.retrocarnage.editor.core.ApplicationFolderService;
@@ -22,44 +20,11 @@ import org.apache.commons.io.IOUtils;
  *
  * @author Thomas Werner
  */
-public class Sprite {
+public class Sprite extends Asset<Sprite> {
 
     private static final Logger logger = Logger.getLogger(Sprite.class.getName());
 
-    private String id;
-    private AttributionData attributionData;
-    private String relativePath;
     private String relativePathThumbnail;
-    private String name;
-    private List<String> tags;
-
-    public Sprite() {
-        this.tags = new ArrayList<>();
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(final String id) {
-        this.id = id;
-    }
-
-    public AttributionData getAttributionData() {
-        return attributionData;
-    }
-
-    public void setAttributionData(final AttributionData attributionData) {
-        this.attributionData = attributionData;
-    }
-
-    public String getRelativePath() {
-        return relativePath;
-    }
-
-    public void setRelativePath(final String relativePath) {
-        this.relativePath = relativePath;
-    }
 
     public String getRelativePathThumbnail() {
         return relativePathThumbnail;
@@ -67,45 +32,6 @@ public class Sprite {
 
     public void setRelativePathThumbnail(final String relativePathThumbnail) {
         this.relativePathThumbnail = relativePathThumbnail;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(final List<String> tags) {
-        this.tags = tags;
-    }
-
-    /**
-     * Reads the file associated with this asset.
-     *
-     * @param out destination
-     * @throws IOException
-     */
-    public void getData(final OutputStream out) throws IOException {
-        final ApplicationFolderService appFolderService = ApplicationFolderService.getDefault();
-        final Path appFolderPath = appFolderService.getApplicationFolder();
-        final Path filePath = Paths.get(appFolderPath.toString(), relativePath);
-        if (filePath.toFile().exists()) {
-            try (final InputStream in = new BufferedInputStream(Files.newInputStream(filePath))) {
-                IOUtils.copy(in, out);
-            } catch (IOException ex) {
-                logger.log(Level.WARNING, "Failed to read sprite asset: {0}", filePath.toString());
-                throw new IOException("Failed to read sprite asset " + filePath.toString(), ex);
-            }
-        } else {
-            logger.log(Level.WARNING, "Missing asset: {0}", filePath.toString());
-            throw new IOException("File not found: " + filePath.toString());
-        }
     }
 
     /**
