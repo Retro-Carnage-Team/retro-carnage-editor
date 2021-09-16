189682aba4c07c9ca8182d83fd5aba0b2d6736c7
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2021-09-16 19:55:17 +0200

    Added code counter

--- cloc.sh
+++ cloc.sh
@@ -1,2 +1,7 @@
+#!/bin/sh
+
+# This runs tokei (https://github.com/XAMPPRocky/tokei) to generate some statistics about the 
+# source code of this project. You might want to adapt the path to the tokei executable. To 
+# run this with every commit, add ". ./cloc.sh" to your pre-commit git hook.
 ~/.cargo/bin/tokei -t "Java,Shell,Markdown,Plain Text" . > tokei.txt
 git add tokei.txt
