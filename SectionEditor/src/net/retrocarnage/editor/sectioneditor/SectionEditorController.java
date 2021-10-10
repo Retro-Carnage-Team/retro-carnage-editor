87b02963e48f177e5aeb29599c3e307fcc5aa7db
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2021-10-10 22:31:50 +0200

    Worked on SectionEditor

--- SectionEditor/src/net/retrocarnage/editor/sectioneditor/SectionEditorController.java
+++ SectionEditor/src/net/retrocarnage/editor/sectioneditor/SectionEditorController.java
@@ -1,7 +1,16 @@
 package net.retrocarnage.editor.sectioneditor;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.JTable;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.AbstractTableModel;
 import net.retrocarnage.editor.model.gameplay.GamePlay;
+import net.retrocarnage.editor.model.gameplay.Section;
 import org.openide.util.Lookup;
 import org.openide.util.LookupEvent;
 import org.openide.util.LookupListener;
@@ -12,28 +21,157 @@ import org.openide.util.Utilities;
  *
  * @author Thomas Werner
  */
-public class SectionEditorController implements LookupListener {
+public class SectionEditorController {
 
-    private Lookup.Result<GamePlay> lookupResult;
+    static final String PROPERTY_SECTIONS = "sections";
+    static final String PROPERTY_SELECTION = "selection";
+
+    private final PropertyChangeSupport propertyChangeSupport;
+    private final LookupListener lookupListener;
+    private final Lookup.Result<GamePlay> lookupResult;
+
+    private Section selectedSection;
+    private List<Section> sections;
+    private SectionTableModel tableModel;
 
     SectionEditorController() {
+        propertyChangeSupport = new PropertyChangeSupport(this);
+        sections = Collections.emptyList();
+
+        lookupListener = (final LookupEvent le) -> handleLookupResultChanged();
         lookupResult = Utilities.actionsGlobalContext().lookupResult(GamePlay.class);
-        lookupResult.addLookupListener(this);
+        lookupResult.addLookupListener(lookupListener);
     }
 
-    @Override
-    public void resultChanged(final LookupEvent le) {
-        Collection<? extends GamePlay> items = lookupResult.allInstances();
-        if (items.isEmpty()) {
-            System.out.println("No gameplay selected");
-        } else {
-            final GamePlay gp = items.iterator().next();
-            System.out.println("GamePlay selected: " + gp.getMissionId());
-        }
+    void addPropertyChangeListener(final PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    void removePropertyChangeListener(final PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
     }
 
     void close() {
-        lookupResult.removeLookupListener(this);
+        lookupResult.removeLookupListener(lookupListener);
+    }
+
+    List<Section> getSections() {
+        return sections;
+    }
+
+    Section getSelectedSection() {
+        return selectedSection;
+    }
+
+    AbstractTableModel getTableModel() {
+        if (null == tableModel) {
+            tableModel = new SectionTableModel();
+        }
+        return tableModel;
+    }
+
+    /**
+     * Builds a ListSelectionListener for the sections table.
+     *
+     * @return the ListSelectionListener
+     */
+    ListSelectionListener getTableSelectionListener(final JTable table) {
+        return (ListSelectionEvent lse) -> {
+            final int selectedRow = table.getSelectedRow();
+            if (!lse.getValueIsAdjusting()) {
+                final Section oldValue = selectedSection;
+                selectedSection = (selectedRow > -1) && (selectedRow < sections.size())
+                        ? sections.get(selectedRow)
+                        : null;
+                propertyChangeSupport.firePropertyChange(PROPERTY_SELECTION, oldValue, selectedSection);
+            }
+        };
+    }
+
+    void increaseLengthOfSection(Section section) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    void decreaseLengthOfSection(Section section) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /**
+     * Adds a new section at the end of the current list of sections
+     */
+    void addSection() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /**
+     * Deletes the selected section - if possible
+     */
+    void deleteSection() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void handleLookupResultChanged() {
+        final Collection<? extends GamePlay> items = lookupResult.allInstances();
+        final List<Section> oldValue = sections;
+        sections = items.isEmpty()
+                ? Collections.emptyList()
+                : items.iterator().next().getSections();
+        propertyChangeSupport.firePropertyChange(PROPERTY_SECTIONS, oldValue, sections);
+
+        if (null != tableModel) {
+            tableModel.fireTableDataChanged();
+        }
+    }
+
+    /**
+     * The TableModel used by the section table.
+     */
+    private class SectionTableModel extends AbstractTableModel {
+
+        final String[] columnNames = {"Direction", "Length", "+", "-"};
+
+        @Override
+        public String getColumnName(int col) {
+            return columnNames[col];
+        }
+
+        @Override
+        public int getRowCount() {
+            return null == sections
+                    ? 0
+                    : sections.size();
+        }
+
+        @Override
+        public int getColumnCount() {
+            return columnNames.length;
+        }
+
+        @Override
+        public Object getValueAt(int row, int column) {
+            if (null == sections || sections.size() <= row) {
+                return null;
+            }
+
+            final Section section = sections.get(row);
+            switch (column) {
+                case 0:
+                    return section.getDirection().getExportName();
+                case 1:
+                    return section.getNumberOfScreens();
+                case 2:
+                    return "+";
+                case 3:
+                    return "-";
+                default:
+                    return "";
+            }
+        }
+
+        @Override
+        public boolean isCellEditable(int row, int col) {
+            return false;
+        }
     }
 
 }
--- SectionEditor/src/net/retrocarnage/editor/sectioneditor/SectionEditorTopComponent.java
+++ SectionEditor/src/net/retrocarnage/editor/sectioneditor/SectionEditorTopComponent.java
@@ -1,5 +1,9 @@
 package net.retrocarnage.editor.sectioneditor;
 
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeEvent;
+import javax.swing.AbstractAction;
+import net.retrocarnage.editor.model.gameplay.Section;
 import org.netbeans.api.settings.ConvertAsProperties;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
@@ -36,6 +40,7 @@ public final class SectionEditorTopComponent extends TopComponent {
 
     public SectionEditorTopComponent() {
         controller = new SectionEditorController();
+        controller.addPropertyChangeListener((PropertyChangeEvent pce) -> handleControllerPropertyChanged(pce));
 
         initComponents();
         setName(Bundle.CTL_SectionEditorTopComponent());
@@ -49,19 +54,93 @@ public final class SectionEditorTopComponent extends TopComponent {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 479, Short.MAX_VALUE)
+        pnlPreview = new javax.swing.JPanel();
+        scrSections = new javax.swing.JScrollPane();
+        tblSections = new javax.swing.JTable();
+        pnlActions = new javax.swing.JPanel();
+        btnAddSection = new javax.swing.JButton();
+        btnRemoveSection = new javax.swing.JButton();
+
+        setLayout(new java.awt.BorderLayout());
+
+        pnlPreview.setPreferredSize(new java.awt.Dimension(411, 411));
+
+        javax.swing.GroupLayout pnlPreviewLayout = new javax.swing.GroupLayout(pnlPreview);
+        pnlPreview.setLayout(pnlPreviewLayout);
+        pnlPreviewLayout.setHorizontalGroup(
+            pnlPreviewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 411, Short.MAX_VALUE)
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 538, Short.MAX_VALUE)
+        pnlPreviewLayout.setVerticalGroup(
+            pnlPreviewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 411, Short.MAX_VALUE)
         );
+
+        add(pnlPreview, java.awt.BorderLayout.PAGE_START);
+
+        tblSections.setModel(controller.getTableModel());
+        tblSections.setRowHeight(24);
+        new ButtonColumn(tblSections, new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                final int modelRow = Integer.valueOf(e.getActionCommand());
+                final Section section = controller.getSections().get(modelRow);
+                controller.increaseLengthOfSection(section);
+            }
+        }, 2);
+
+        new ButtonColumn(tblSections, new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                final int modelRow = Integer.valueOf(e.getActionCommand());
+                final Section section = controller.getSections().get(modelRow);
+                controller.decreaseLengthOfSection(section);
+            }
+        }, 3);
+
+        tblSections
+        .getSelectionModel()
+        .addListSelectionListener(controller.getTableSelectionListener(tblSections));
+        scrSections.setViewportView(tblSections);
+
+        add(scrSections, java.awt.BorderLayout.CENTER);
+
+        pnlActions.setLayout(new java.awt.GridLayout());
+
+        org.openide.awt.Mnemonics.setLocalizedText(btnAddSection, org.openide.util.NbBundle.getMessage(SectionEditorTopComponent.class, "SectionEditorTopComponent.btnAddSection.text")); // NOI18N
+        btnAddSection.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddSectionActionPerformed(evt);
+            }
+        });
+        pnlActions.add(btnAddSection);
+
+        org.openide.awt.Mnemonics.setLocalizedText(btnRemoveSection, org.openide.util.NbBundle.getMessage(SectionEditorTopComponent.class, "SectionEditorTopComponent.btnRemoveSection.text")); // NOI18N
+        btnRemoveSection.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRemoveSectionActionPerformed(evt);
+            }
+        });
+        pnlActions.add(btnRemoveSection);
+
+        add(pnlActions, java.awt.BorderLayout.PAGE_END);
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnAddSectionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddSectionActionPerformed
+        controller.addSection();
+    }//GEN-LAST:event_btnAddSectionActionPerformed
+
+    private void btnRemoveSectionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRemoveSectionActionPerformed
+        controller.deleteSection();
+    }//GEN-LAST:event_btnRemoveSectionActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAddSection;
+    private javax.swing.JButton btnRemoveSection;
+    private javax.swing.JPanel pnlActions;
+    private javax.swing.JPanel pnlPreview;
+    private javax.swing.JScrollPane scrSections;
+    private javax.swing.JTable tblSections;
     // End of variables declaration//GEN-END:variables
     @Override
     public void componentOpened() {
@@ -74,14 +153,20 @@ public final class SectionEditorTopComponent extends TopComponent {
     }
 
     void writeProperties(java.util.Properties p) {
-        // better to version settings since initial version as advocated at
-        // http://wiki.apidesign.org/wiki/PropertyFiles
         p.setProperty("version", "1.0");
-        // TODO store your settings
     }
 
     void readProperties(java.util.Properties p) {
-        String version = p.getProperty("version");
-        // TODO read your settings according to their version
+        // String version = p.getProperty("version");
+    }
+
+    private void handleControllerPropertyChanged(final PropertyChangeEvent pce) {
+        switch (pce.getPropertyName()) {
+            case SectionEditorController.PROPERTY_SELECTION:
+                btnRemoveSection.setEnabled(null != pce.getNewValue());
+                break;
+            case SectionEditorController.PROPERTY_SECTIONS:
+                break;
+        }
     }
 }
