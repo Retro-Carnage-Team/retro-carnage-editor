d1f2c59006ef7566f013604c91ad51ab21b16c5c
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2024-10-01 09:51:48 +0200

    Changed type of action listener to fix non-deterministic behavior

--- EditorNodes/src/net/retrocarnage/editor/nodes/nodes/EnemyChildren.java
+++ EditorNodes/src/net/retrocarnage/editor/nodes/nodes/EnemyChildren.java
@@ -1,12 +1,10 @@
 package net.retrocarnage.editor.nodes.nodes;
 
 import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import net.retrocarnage.editor.model.Enemy;
 import net.retrocarnage.editor.model.Layer;
 import org.openide.nodes.Children;
 import org.openide.nodes.Node;
-import org.openide.util.WeakListeners;
 
 /**
  * Creates EnemyNode for each Enemy contained in the specified Layer.
@@ -14,13 +12,12 @@ import org.openide.util.WeakListeners;
  * @author Thomas Werner
  */
 public final class EnemyChildren extends Children.Keys<Enemy> {
-
+    
     private final Layer layer;
 
     public EnemyChildren(final Layer layer) {
         this.layer = layer;
-        final ChangeListener listener = (ChangeEvent ce) -> addNotify();
-        layer.getEnemies().addChangeListener(WeakListeners.change(listener, layer.getEnemies()));
+        layer.getEnemies().addChangeListener((ChangeEvent ce) -> addNotify());
     }
 
     @Override
